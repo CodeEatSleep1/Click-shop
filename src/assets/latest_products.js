f6e9f1128495adcde39a24b09bd26306e4ba720a
Rayyan Sheikh <m.ahsanansari2022@gmail.com>
2024-11-26 07:08:27 +0500

    Update latest_products.js

--- src/assets/latest_products.js
+++ src/assets/latest_products.js
@@ -1,7 +1,7 @@
 const latestProducts=[
     {
       "id": 1,
-      "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+      "title": "Fjallraven - Foldsack No. 1 Backpack",
       "price": 109.95,
       "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
       "category": "men's clothing",
@@ -13,7 +13,7 @@ const latestProducts=[
     },
     {
       "id": 17,
-      "title": "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+      "title": "Rain Jacket Women Windbreaker.",
       "price": 39.99,
       "description": "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
       "category": "women's clothing",
@@ -25,7 +25,7 @@ const latestProducts=[
     },
     {
       "id": 20,
-      "title": "DANVOUY Womens T Shirt Casual Cotton Short",
+      "title": "DANVOUY Womens T Shirt Casual.",
       "price": 12.99,
       "description": "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
       "category": "women's clothing",
@@ -37,7 +37,7 @@ const latestProducts=[
     },
     {
       "id": 14,
-      "title": "Samsung 49-Inch CHG90 144Hz Curved Gaming",
+      "title": "Samsung 49-Inch CHG90 144Hz.",
       "price": 999.99,
       "description": "49 INCH SUPER ULTRAWIDE 32:9 CURVED GAMING MONITOR with dual 27 inch screen side by side QUANTUM DOT (QLED) TECHNOLOGY, HDR support and factory calibration provides stunningly realistic and accurate color and contrast 144HZ HIGH REFRESH RATE and 1ms ultra fast response time work to eliminate motion blur, ghosting, and reduce input lag",
       "category": "electronics",
@@ -49,7 +49,7 @@ const latestProducts=[
     },
     {
       "id": 13,
-      "title": "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
+      "title": "Acer SB220Q bi 21.5 inches Full HD.",
       "price": 599,
       "description": "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
       "category": "electronics",
@@ -85,7 +85,7 @@ const latestProducts=[
     },
     {
       "id": 5,
-      "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+      "title": "John Hardy Women's Naga Gold & Silver.",
       "price": 695,
       "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
       "category": "jewelery",
