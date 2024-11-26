6b6ee741109adde6327397ae3f9a621974ae564a
Rayyan Sheikh <m.ahsanansari2022@gmail.com>
2024-11-26 07:13:56 +0500

    Update trending_products.js

--- src/assets/trending_products.js
+++ src/assets/trending_products.js
@@ -25,7 +25,7 @@ const trendingProducts=[
       },
       {
         "id": 5,
-        "title": "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+        "title": "John Hardy Women's Legends Naga Gold & Silver.",
         "price": 695,
         "description": "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
         "category": "jewelery",
@@ -49,7 +49,7 @@ const trendingProducts=[
       },
       {
         "id": 13,
-        "title": "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
+        "title": "Acer SB220Q bi 21.5 inches Full HD.",
         "price": 599,
         "description": "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
         "category": "electronics",
@@ -74,7 +74,7 @@ const trendingProducts=[
       
     {
       "id": 17,
-      "title": "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+      "title": "Rain Jacket Women Windbreaker.",
       "price": 39.99,
       "description": "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
       "category": "women's clothing",
